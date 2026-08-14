b833f796f970061a3689ab9ab1f16ecd2bfceed8
EstherAuma <estheraumaego@gmail.com>
2026-08-14 14:48:57 +0300

    create project

---
+++ src/app/add-task/add-task.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-task',
+  templateUrl: './add-task.page.html',
+  styleUrls: ['./add-task.page.scss'],
+  standalone: false
+})
+export class AddTaskPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,10 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'add-task',
+    loadChildren: () => import('./add-task/add-task.module').then( m => m.AddTaskPageModule)
   }
 ];
 
